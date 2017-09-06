7d3de7f88e6c82e970d82e7c8f654d365a685aa1
Dylan Alvarez <dylanalvarez1995@gmail.com>
2017-09-06 19:12:24 -0300

    make a shitty prototype parser of input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define SUCCESS 0
+#define ERROR 1
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) { return ERROR; }
+    int current = 1;
+
+    bool debug = std::string(argv[current]) == "--debug";
+    if (debug) {
+        current++;
+        std::cout << "debug mode!" << std::endl;
+    }
+
+    if (current >= argc) { return ERROR; }
+    if (std::string(argv[current]) == "--input") {
+        current++;
+        if (current >= argc) { return ERROR; }
+        std::cout << "read from file " << argv[current] << std::endl;
+        current++;
+    }
+
+    if (current >= argc) { return ERROR; }
+    if (std::string(argv[current]) == "--output") {
+        current++;
+        if (current >= argc) { return ERROR; }
+        std::cout << "write to file " << argv[current] << std::endl;
+        current++;
+    }
+
+    std::cout << "rest of parameters:" << std::endl;
+
+    while(current < argc){
+        std::cout << argv[current] << std::endl;
+        current++;
+    }
+
+    std::cout << "success!" << std::endl;
+    return SUCCESS;
 }
